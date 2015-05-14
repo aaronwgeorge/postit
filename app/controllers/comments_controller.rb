3de32c314145c15b779bd7fa232284aba94a5838
Aaron George <aaronwgeorge@gmail.com>
2015-05-14 15:15:46 -0700

    fixed bug with voting on comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,7 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find_by(slug: params[:post_id])
-    @comment = Comment.new(params.require(:comment).permit(:body))
-    @comment.post = @post
+    @comment = @post.comments.build(params.require(:comment).permit(:body))
     @comment.user = current_user
 
     if @comment.save
